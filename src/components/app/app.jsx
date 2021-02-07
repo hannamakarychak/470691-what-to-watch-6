b1f6fabe25652b4506ba5f28f043c1c36b2421e9
hannamakarychak <annamakarichak3@gmail.com>
2021-02-07 18:57:24 +0100

    create all pages components, create routing for all pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import SignInPage from '../sign-in-page/sign-in-page';
 import MainPage from '../main-page/main-page';
+import MyListPage from '../my-list-page/my-list-page';
+import MoviePage from '../movie-page/movie-page';
+import AddReviewPage from '../add-review-page/add-review-page';
+import PlayerPage from '../player-page/player-page';
+
 
 const App = ({title, genre, releaseDate}) => {
-  return <MainPage title={title} genre={genre} releaseDate={releaseDate} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage title={title} genre={genre} releaseDate={releaseDate} />
+        </Route>
+        <Route exact path="/login">
+          <SignInPage />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListPage />
+        </Route>
+        <Route exact path="/films/:id">
+          <MoviePage />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewPage />
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerPage />
+        </Route>
+        <Route
+          render={() => (
+            <Fragment>
+              <div className="user-page">
+                <header className="page-header user-page__head">
+                  <div className="logo">
+                    <a href="/" className="logo__link">
+                      <span className="logo__letter logo__letter--1">W</span>
+                      <span className="logo__letter logo__letter--2">T</span>
+                      <span className="logo__letter logo__letter--3">W</span>
+                    </a>
+                  </div>
+                </header>
+                <h1 className="page-title user-page__title">404.<br></br>
+                  <small>Page not found</small>
+                </h1>
+              </div>
+            </Fragment>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
---
+++ src/components/my-list-page/my-list-page.jsx
@@ -0,0 +1,55 @@
+import React, {Fragment} from 'react';
+
+import Card from '../card/card';
+
+const MyListPage = () => {
+  const cardsList = new Array(9).fill();
+  return (
+    <Fragment>
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="/" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <h1 className="page-title user-page__title">My list</h1>
+
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </div>
+          </div>
+        </header>
+
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+          <div className="catalog__movies-list">
+            {cardsList.map((el, index) => <Card key={index} />)}
+          </div>
+        </section>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </Fragment>
+  );
+};
+
+export default MyListPage;
+
